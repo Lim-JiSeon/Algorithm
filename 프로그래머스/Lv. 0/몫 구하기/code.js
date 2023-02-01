e1921340128c7ecfd862a589b923c8996c10d133
Lim-JiSeon <83554018+Lim-JiSeon@users.noreply.github.com>
2023-02-01 20:37:01 +0900

    Create code.js

---
+++ 프로그래머스/Lv. 0/몫 구하기/code.js
@@ -0,0 +1,11 @@
+function divide(num1, num2) {
+    return num1/num2;
+}
+
+function getQuotient(num1, num2) {
+    return parseInt(divide(num1, num2));
+}
+
+function solution(num1, num2) {
+    return getQuotient(num1, num2);
+}
