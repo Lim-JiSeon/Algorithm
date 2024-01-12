dc92d101961175ecbdeb760c5348668d22950403
Lim-JiSeon <83554018+Lim-JiSeon@users.noreply.github.com>
2024-01-13 08:21:03 +0900

    [level Lv.2] Title: 멀리 뛰기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var dp = new Array(n + 1)
+    
+    dp[0] = 1
+    dp[1] = 2
+    
+    for (let i = 2; i < n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
+    }
+    
+    return dp[n - 1];
+}
