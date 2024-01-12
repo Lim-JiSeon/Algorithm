4ca7f3403716c11352f4225de092d724def22b87
Lim-JiSeon <83554018+Lim-JiSeon@users.noreply.github.com>
2024-01-13 04:59:08 +0900

    [level Lv.2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,14 @@
+function solution(n)
+{
+    var ans = n % 2 ? 1 : 0;
+
+    while(n > 1) {
+        n = parseInt(n / 2)
+        if (n % 2) {
+            ans += 1
+            n -= 1
+        }
+    }
+
+    return ans;
+}
